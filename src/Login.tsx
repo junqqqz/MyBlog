29d0fb41205e04ac6c8f7f5c7072744ff572f6f6
jun qiang <2242343946@qq.com>
2024-05-22 22:17:32 +0800

    'xxx'

---
+++ src/Login.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="header">111</div>
+      <div className="container">
+        <div className="info">
+          <div>在线投票 | 调查问卷</div>
+            <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
